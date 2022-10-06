3a22aa44ff4e18ef6653d2fe42709d2651fc76e6
Deadpool1 <atwood169@yahoo.com>
2022-10-05 19:57:13 -0500

    added project 3

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,12 +3,13 @@ import React from "react";
 function Project() {
     const projects = [
         {
-            name: "Take A Hike",
-            description: "A Fun and easy to use application to view and share your very own hiking experience.",
-            image: "take-a-hike.jpeg",
-            technologies: "JavaScript, Handlebars.js",
-            github: "https://github.com/willig0203/Take-A-Hike#usage",
-            deployed: "https://taka-hike.herokuapp.com/",
+          name: "Exam Cram",
+          description: "This is a web application for users to create and view flashcards",
+          image: "",
+          technologies: "JavaScript, MongoDB, Express.js, React, Node.js",
+          github: "https://github.com/lundbmp/flash-cards",
+          deployed: "https://exam-cram.herokuapp.com//",
+      
         },
         {
             name: "Take A Hike",
